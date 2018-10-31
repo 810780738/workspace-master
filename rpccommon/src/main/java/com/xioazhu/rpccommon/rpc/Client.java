eefdd9810f0d4ce4307d69e7392c6dcee3680ab0
zhusm <810780738@qq.com>
2018-10-31 09:41:41 +0800

    新增RPC请求方式

--- rpccommon/src/main/java/com/xioazhu/rpccommon/rpc/Client.java
+++ rpccommon/src/main/java/com/xioazhu/rpccommon/rpc/Client.java
@@ -12,4 +12,10 @@ package com.xioazhu.rpccommon.rpc;/**
  *@Version 1.0
  */
 public class Client {
+
+
+    public static <T> T rpcInvoke(String beanName,String methodName,Object... paramters){
+
+        return null;
+    }
 }
---
+++ rpccommon/src/main/java/com/xioazhu/rpccommon/rpc/ContextUtils.java
@@ -0,0 +1,16 @@
+package com.xioazhu.rpccommon.rpc;/**
+ * @Auther: Administrator
+ * @Date: 2018/10/30 19:45
+ * @Description:
+ */
+
+/**
+ *@ClassName ContextUtils
+ *@Description TODO
+ *@Author zhusm
+ *@Date 2018/10/30 19:45    
+ *@Version 1.0
+ */
+public class ContextUtils {
+//    private static Th
+}
